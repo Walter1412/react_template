3cb19f6f5d1f6f0937077f2f98eb20ec8cb97218
Walter <walter@medialand.tw>
2019-10-29 02:46:54 +0800

    css and sass test

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import styles from "./style.module.scss";
 function About() {
-  return <h1>Hello About</h1>;
+  return <h1 className={styles.error}>Hello About</h1>;
 }
 export default About;
