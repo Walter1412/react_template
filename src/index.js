02fcf440bcc23c0e1cc594e0e1d59c9781a5f0ab
Walter <walter@medialand.tw>
2019-10-29 02:35:50 +0800

    react  router test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./views/app";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./views/home";
+import About from "./views/about";
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
