a2fb4063683e0263eed0e353cd66a8aabcc03728
Walter <walter@medialand.tw>
2020-01-22 03:56:13 +0800

    loadable

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import { Switch, Route, Redirect, useLocation } from 'react-router-dom'
+import Loadable from '@loadable/component'
 import { cloneDeep } from 'lodash'
 import { Wrapper, Container } from './style'
-import Home from './js/views/home'
-import Login from './js/views/login'
+
+const loading = () => {
+	return <div>Loading...</div>
+}
+
+const Home = Loadable(() => import('./js/views/home'), {
+	loading,
+})
+const Login = Loadable(() => import('./js/views/login'), {
+	loading,
+})
 
 function App() {
 	const { pathname } = useLocation()
@@ -12,12 +22,8 @@ function App() {
 		<Wrapper pageName={cloneDeep(pathname).replace('/', '')}>
 			<Container>
 				<Switch>
-					<Route strict sensitive path='/login'>
-						<Login />
-					</Route>
-					<Route strict sensitive path='/home'>
-						<Home />
-					</Route>
+					<Route strict sensitive path='/login' component={Login} />
+					<Route strict sensitive path='/home' component={Home} />
 					<Redirect push from='/' to='/home' />
 				</Switch>
 			</Container>
