2ed07a8ee3c8b553d273609c480592a0bdeb8463
CI-Walter <walter.huang@cloud-interactive.com>
2020-02-17 18:27:55 +0800

    final form test

--- src/js/views/login/components/Form/index.js
+++ src/js/views/login/components/Form/index.js
@@ -1,68 +1,21 @@
 import React from 'react'
 import styles from './style.module.scss'
 import classNames from 'classnames/bind'
-import { Form, Field } from 'react-final-form'
+import { Form } from 'components/common'
+import { Form as FinalForm, Field as FinalField } from 'react-final-form'
 const cx = classNames.bind(styles)
 
 function LoginForm(props) {
 	const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 	const onSubmit = async values => {
-		await sleep(300)
 		window.alert(JSON.stringify(values, 0, 2))
 	}
 
 	const validate = values => {
 		const errors = {}
-		if (!values.userName) {
-			errors.userName = 'Required'
-		}
-		if (!values.password) {
-			errors.password = 'Required'
-		}
 		return errors
 	}
-	return (
-		<Form
-			onSubmit={onSubmit}
-			validate={validate}
-			render={({ handleSubmit, form, submitting, pristine }) => (
-				<form onSubmit={handleSubmit}>
-					<Field name='userName'>
-						{({ input, meta }) => {
-							return (
-								<div>
-									<label>UserName</label>
-									<input {...input} type='text' placeholder='userName' />
-									{meta.error && meta.touched && <span>{meta.error}</span>}
-								</div>
-							)
-						}}
-					</Field>
-					<Field name='password'>
-						{({ input, meta }) => (
-							<div>
-								<label>Password</label>
-								<input {...input} type='text' placeholder='password' />
-								{meta.error && meta.touched && <span>{meta.error}</span>}
-							</div>
-						)}
-					</Field>
-					<div className='buttons'>
-						<button type='submit' disabled={submitting}>
-							Submit
-						</button>
-						<button
-							type='button'
-							onClick={form.reset}
-							disabled={submitting || pristine}
-						>
-							Reset
-						</button>
-					</div>
-				</form>
-			)}
-		/>
-	)
+	return <FinalForm onSubmit={onSubmit}>{}</FinalForm>
 }
 
 export default LoginForm
